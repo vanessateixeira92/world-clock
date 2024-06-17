17bc626b30e9503c1bb9ac2ad1e024c6b89236ef
vanessateixeira92 <vanessa.r.m.teixeira@gmail.com>
2024-06-17 02:37:39 +0100

    Added another city

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 function updateTime() {
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
